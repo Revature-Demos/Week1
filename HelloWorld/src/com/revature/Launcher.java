127f6af5951a07939464943c212d7e78ce032761
juandiazflores <70529036+juandiazflores@users.noreply.github.com>
2024-10-01 09:30:22 -0400

    moved HelloWorld into week1

---
+++ HelloWorld/src/com/revature/Launcher.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+public class Launcher  {
+    // This is the main method
+    public static void main(String[] args) {
+        // This is a print statement
+        // It allows us to print a message to a console (the box that appears when we run the app)
+        System.out.println("Hello\" world!");
+        /* This is a block comment AKA multi-line comment
+
+        System = This system. The console.
+        out = output, we're defining something that will be outputted.
+        println = prints a line to the console
+
+        fun fact: if we just did System.out.print, they would all print on the same line
+
+        */
+    }
+}
